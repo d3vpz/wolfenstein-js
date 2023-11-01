078f2e0b57027930411418680f9df97c9131d04e
Devpz <114014131+d3vpz@users.noreply.github.com>
2023-11-01 21:51:01 +0000

    textures

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wolfenstein-js
 A Wolfenstein 3D engine made in JavaScript.
+Textures ripped by Ultimecia
--- main.js
+++ main.js
@@ -10,20 +10,20 @@ var fps_rate='...';
 
 // map
 const MAP_SIZE=10;
-const MAP_SCALE=10;
+const MAP_SCALE=64;
 const MAP_RANGE=MAP_SIZE*MAP_SCALE;
 const MAP_SPEED=(MAP_SCALE/2)/10;
 var map=[
-    1,1,1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,1,1,1,
-    1,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,1,
-    1,0,0,0,1,1,0,0,0,1,
-    1,0,0,0,0,0,0,0,0,1,
-    1,0,0,1,0,0,0,0,0,1,
-    1,0,0,1,0,0,0,1,0,1,
-    1,1,1,1,1,1,1,1,1,1,
+    3,3,3,3,3,3,3,3,3,3,
+    3,0,0,0,0,0,0,0,0,3,
+    3,0,0,0,0,0,0,1,1,3,
+    3,0,0,0,0,0,0,0,0,3,
+    3,0,0,0,0,0,0,0,0,3,
+    3,0,0,0,2,1,0,0,0,3,
+    3,0,0,0,0,0,0,0,0,3,
+    3,0,0,1,0,0,0,0,0,3,
+    3,0,0,1,0,0,0,1,0,3,
+    3,3,3,3,3,3,3,3,3,3,
 ];
 var show_map=false;
 
@@ -52,7 +52,6 @@ var player={
 
 // handle user input
 document.onkeydown=function(e){
-    console.log(e.key);
     switch(e.key){
         case'ArrowDown':player.move_x=-1;player.move_y=-1;break;
         case'ArrowUp':player.move_x=1;player.move_y=1;break;
@@ -72,6 +71,19 @@ document.onkeyup=function(e){
     }
 };
 
+// graphics
+const TEXTURE_COUNT=4;
+const TEXTURES=[];
+
+// load wall textures
+for(var file=0;file<TEXTURE_COUNT;file++){
+    var image=document.createElement('img');
+    image.src='assets/images/'+file+'.png';
+    TEXTURES.push(image);
+}
+
+console.log(TEXTURES);
+
 // game loop
 function game_loop(){
     // calculate FPS
@@ -89,9 +101,14 @@ function game_loop(){
 
     context.beginPath();
 
-    // update screen
-    context.fillStyle='black';
-    context.fillRect(canvas.width/2-HALF_WIDTH,canvas.height/2-HALF_HEIGHT,WIDTH,HEIGHT);
+    // draw background
+    context.drawImage(
+        TEXTURES[0],
+        0,1152,
+        64,64,
+        canvas.width/2-HALF_WIDTH,canvas.height/2-HALF_HEIGHT,
+        WIDTH,HEIGHT
+    );
 
     // update player position
     var player_offset_x=Math.sin(player.angle)*MAP_SPEED;
@@ -104,8 +121,8 @@ function game_loop(){
     if(player.move_angle)player.angle+=0.06*player.move_angle;
 
     // calculate map and player offsets
-    let offset_x=Math.floor(canvas.width/2)-149;
-    let offset_y=Math.floor(canvas.height/2)-99;
+    let offset_x=Math.floor(canvas.width/2)-HALF_WIDTH;
+    let offset_y=Math.floor(canvas.height/2)-HALF_HEIGHT;
     var player_map_x=player.x+offset_x;
     var player_map_y=player.y+offset_y;
 
@@ -121,7 +138,7 @@ function game_loop(){
         var current_cos=Math.cos(current_angle);current_cos=current_cos?current_cos:0.000001;
 
         // vertical line intersection
-        var ray_end_x,ray_end_y,ray_dir_x,vertical_depth;
+        var ray_end_x,ray_end_y,ray_dir_x,vertical_depth,texture_end_y,texture_y;
         if(current_sin>0){ray_end_x=ray_start_x+MAP_SCALE;ray_dir_x=1;}
         else{ray_end_x=ray_start_x;ray_dir_x=-1;}
         for(var offset=0;offset<MAP_RANGE;offset+=MAP_SCALE){
@@ -132,12 +149,12 @@ function game_loop(){
             if(current_sin<=0)map_target_x+=ray_dir_x;
             var target_square=map_target_y*MAP_SIZE+map_target_x;
             if(target_square<0||target_square>map.length-1)break;
-            if(map[target_square]!=0)break;
+            if(map[target_square]!=0){texture_y=map[target_square];break;}
             ray_end_x+=ray_dir_x*MAP_SCALE;
-        }
+        }texture_end_y=ray_end_y;
 
         // horizontal line intersection
-        var ray_end_y,ray_end_x,ray_dir_y,horiz_depth;
+        var ray_end_y,ray_end_x,ray_dir_y,horiz_depth,texture_end_x,texture_x;
         if(current_cos>0){ray_end_y=ray_start_y+MAP_SCALE;ray_dir_y=1;}
         else{ray_end_y=ray_start_y;ray_dir_y=-1;}
         for(var offset=0;offset<MAP_RANGE;offset+=MAP_SCALE){
@@ -148,20 +165,32 @@ function game_loop(){
             if(current_cos<=0)map_target_y+=ray_dir_y;
             var target_square=map_target_y*MAP_SIZE+map_target_x;
             if(target_square<0||target_square>map.length-1)break;
-            if(map[target_square]!=0)break;
+            if(map[target_square]!=0){texture_x=map[target_square];break};
             ray_end_y+=ray_dir_y*MAP_SCALE;
-        }
+        }texture_end_x=ray_end_x;
 
         // render 3D projection
         var depth=vertical_depth<horiz_depth?depth=vertical_depth:depth=horiz_depth;
+        var texture_image=vertical_depth<horiz_depth?texture_y:texture_x;
+        var texture_offset=vertical_depth<horiz_depth?texture_end_y:texture_end_x;
+        texture_offset=Math.floor(texture_offset-Math.floor(texture_offset/MAP_SCALE)*MAP_SCALE);
         depth*=Math.cos(player.angle-current_angle);
 
-        var wall_height=Math.min(MAP_SCALE*300/(depth+0.0001),HEIGHT);
-        context.beginPath();
-        context.strokeStyle=vertical_depth<horiz_depth?'#aaa':'#555';
-        context.moveTo(ray+offset_x,(HALF_HEIGHT-wall_height/2)+offset_y);
-        context.lineTo(ray+offset_x,(HALF_HEIGHT-wall_height/2)+wall_height+offset_y);
-        context.stroke();
+        var wall_height=Math.min(Math.floor(MAP_SCALE*300/(depth+0.0001)),50000);
+        // context.beginPath();
+        // context.strokeStyle=vertical_depth<horiz_depth?'darkgray':'gray';
+        // context.lineWidth=2;
+        // context.moveTo(ray+offset_x,(HALF_HEIGHT-wall_height/2)+offset_y);
+        // context.lineTo(ray+offset_x,(HALF_HEIGHT-wall_height/2)+wall_height+offset_y);
+        // context.stroke();
+
+        context.drawImage(
+            TEXTURES[texture_image],
+            texture_offset,0,
+            1,64,
+            ray+offset_x,(HALF_HEIGHT-Math.floor(wall_height/2))+offset_y,
+            1,wall_height,
+        );
 
         // update current angle
         current_angle-=STEP_ANGLE;
@@ -175,33 +204,39 @@ function game_loop(){
                 if(map[square]){
                     context.fillStyle='white';
                     context.fillRect(
-                        offset_x+col*MAP_SCALE,
-                        offset_y+row*MAP_SCALE,
-                        MAP_SCALE,MAP_SCALE
+                        offset_x+col*5,
+                        offset_y+row*5,
+                        5,5
                         );
                 }else{
                     context.fillStyle='gray';
                     context.fillRect(
-                        offset_x+col*MAP_SCALE,
-                        offset_y+row*MAP_SCALE,
-                        MAP_SCALE,MAP_SCALE
+                        offset_x+col*5,
+                        offset_y+row*5,
+                        5,5
                         );
                 }
             }
         }
 
         // draw player on 2d map
-        context.beginPath();
-        context.fillStyle='red';
-        context.arc(player_map_x,player_map_y,2,0,DOUBLE_PI);
-        context.fill();
+        // context.beginPath();
+        // context.fillStyle='red';
+        // context.arc(player_map_x,player_map_y,2,0,DOUBLE_PI);
+        // context.fill();
     }
+
+    // fix wall bounds
+    context.beginPath();
+    context.fillStyle='black';
+    context.fillRect(0,0,canvas.width,offset_y);
+    context.fillRect(0,offset_y+HEIGHT,canvas.width,canvas.width-offset_y+200)
 
     // infinite loop
     setTimeout(game_loop,cycle_delay);
 
     //render FPS to screen
-    context.fillStyle='black';
+    context.fillStyle='white';
     context.font='12px Monospace';
     context.fillText('FPS: '+fps_rate,0,20);
 
